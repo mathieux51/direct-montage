2cd07036800da84495155e2158625cd610d8ddd5
Mathieu <mathieux51@gmail.com>
2025-08-06 10:48:07 +0200

    add basePath

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  basePath: '/montage',
   reactStrictMode: false,
   webpack: (config, { isServer }) => {
     config.experiments = {
